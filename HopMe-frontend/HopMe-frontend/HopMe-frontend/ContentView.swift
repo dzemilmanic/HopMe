2e3fcc1cecd7d1e36df279b4618b249cc75ab707
dzemilmanic <manicdzemil@gmail.com>
2025-12-19 15:31:43 +0100

    frontend init

---
+++ HopMe-frontend/HopMe-frontend/HopMe-frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HopMe-frontend
+//
+//  Created by user290734 on 12/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HopMe-frontend/HopMe-frontend/HopMe-frontend/HopMe_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopMe_frontendApp.swift
+//  HopMe-frontend
+//
+//  Created by user290734 on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HopMe_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
