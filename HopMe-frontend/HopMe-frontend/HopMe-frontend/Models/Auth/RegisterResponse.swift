3942265ad112093977ae32801ad208a7ef362bfa
dzemil <dzemilmanic@hotmail.com>
2025-12-26 20:07:45 +0100

    fix

--- HopMe-frontend/HopMe-frontend/HopMe-frontend/Models/Auth/RegisterResponse.swift
+++ HopMe-frontend/HopMe-frontend/HopMe-frontend/Models/Auth/RegisterResponse.swift
@@ -1,25 +1,33 @@
 import Foundation
 
-struct RegisterResponse: Codable {
+struct RegisterResponse: Decodable {
     let message: String
     let userId: Int?
     let vehicleId: Int?
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        message = try container.decode(String.self, forKey: .message)
-        // Try both camelCase and snake_case for userId
-        userId = try container.decodeIfPresent(Int.self, forKey: .userId) ?? 
-                 try container.decodeIfPresent(Int.self, forKey: .userIdSnake)
-        vehicleId = try container.decodeIfPresent(Int.self, forKey: .vehicleId) ??
-                    try container.decodeIfPresent(Int.self, forKey: .vehicleIdSnake)
-    }
-    
+
     enum CodingKeys: String, CodingKey {
         case message
         case userId
         case userIdSnake = "user_id"
         case vehicleId
         case vehicleIdSnake = "vehicle_id"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        message = try container.decode(String.self, forKey: .message)
+
+        if let id = try container.decodeIfPresent(Int.self, forKey: .userId) {
+            userId = id
+        } else {
+            userId = try container.decodeIfPresent(Int.self, forKey: .userIdSnake)
+        }
+
+        if let vId = try container.decodeIfPresent(Int.self, forKey: .vehicleId) {
+            vehicleId = vId
+        } else {
+            vehicleId = try container.decodeIfPresent(Int.self, forKey: .vehicleIdSnake)
+        }
     }
 }
