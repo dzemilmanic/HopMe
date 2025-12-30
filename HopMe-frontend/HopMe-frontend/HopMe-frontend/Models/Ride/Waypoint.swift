9974938c05e0a6a09456451dba65dcdc303fe2f9
Džemil Manić <dzemilmanic@hotmail.com>
2025-12-30 21:39:29 +0100

    ride

--- HopMe-frontend/HopMe-frontend/HopMe-frontend/Models/Ride/Waypoint.swift
+++ HopMe-frontend/HopMe-frontend/HopMe-frontend/Models/Ride/Waypoint.swift
@@ -10,9 +10,6 @@ struct Waypoint: Codable, Identifiable {
     let estimatedTime: Date?
     
     enum CodingKeys: String, CodingKey {
-        case id, location, lat, lng
-        case rideId = "ride_id"
-        case orderIndex = "order_index"
-        case estimatedTime = "estimated_time"
+        case id, location, lat, lng, rideId, orderIndex, estimatedTime
     }
 }
